caa6f83e19bef74c76e156379674b99965b82042
ChandraShekhar <csyadav0513@gmail.com>
2025-01-13 13:21:56 +0530

    add PrismaClient instance management in prisma.ts

---
+++ lib/prisma.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+
+// This is a global variable that will be used to store the PrismaClient instance
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient | undefined;
+};
+
+// If the global variable already has a PrismaClient instance, use that one
+export const prisma = globalForPrisma.prisma || new PrismaClient();
