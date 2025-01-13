0d56c94aa72958fc6f1464f1532e66a82a91c50e
ChandraShekhar <csyadav0513@gmail.com>
2025-01-13 15:24:02 +0530

    implement email verification process in sign-up component

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import { useSignUp } from "@clerk/nextjs";
@@ -7,7 +8,7 @@ import { useState } from "react";
 
 const SignUp = () => {
   const { isLoaded, signUp, setActive } = useSignUp();
-  const [email, setEmail] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
   const [password, setPassword] = useState("");
   const [pendingVerification, setPendingVerification] = useState(false);
   const [code, setCode] = useState("");
@@ -17,6 +18,53 @@ const SignUp = () => {
 
   if (!isLoaded) {
     return <div>Loading...</div>;
+  }
+
+  async function submit(e: React.FormEvent) {
+    e.preventDefault();
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+    try {
+      await signUp.create({ emailAddress, password });
+
+      await signUp.prepareEmailAddressVerification({
+        strategy: "email_code",
+      });
+
+      setPendingVerification(true);
+    } catch (e: any) {
+      console.log(JSON.stringify(e, null, 2));
+      setError(e.message[0].message);
+    }
+  }
+
+  async function onPressVerify(e: React.FormEvent) {
+    e.preventDefault();
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+    try {
+      const completeSignUp = await signUp.attemptEmailAddressVerification({
+        code,
+      });
+
+      if (completeSignUp.status !== "complete") {
+        setError("Verification failed");
+        console.log(JSON.stringify(completeSignUp, null, 2));
+
+        return;
+      }
+
+      if (completeSignUp.status === "complete") {
+        await setActive({
+          session: completeSignUp.createdSessionId,
+        });
+        router.push("/");
+      }
+    } catch (e: any) {
+      setError(e.message[0].message);
+    }
   }
 
   return <div>SignUp</div>;
